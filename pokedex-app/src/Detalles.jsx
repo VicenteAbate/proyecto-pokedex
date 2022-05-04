b1ff971ec5760adfa447e0531b59ad0c9aa7573a
natmacira <natimciraolo@gmail.com>
2022-05-04 16:01:31 -0300

    creación variables de colores y primer llamada al array de pokemons

--- pokedex-app/src/Detalles.jsx
+++ pokedex-app/src/Detalles.jsx
@@ -1,4 +1,41 @@
+import { useEffect, useState } from "react";
+import Pokemons from "./pokemons";
+
 const Detalles =()=> {
+
+    const [listaPokemons, setListaPokemons] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+    
+            try {
+            const response = Pokemons
+        
+            setListaPokemons(response)
+             console.log(response)
+        
+            } catch (error) {
+                 console.log("Hay un error en tu API")   
+                
+            }      
+                   
+            }
+            getData();
+        }, []);
+
+        return(
+            <>
+            <h1>hola</h1>
+            {/* <div>{response}</div>
+            <ul>{listaPokemons &&
+        listaPokemons.map((item, i) => {
+            <li key={i}>{item.Name}</li>
+        })}
+        </ul> */}
+            </>
+        )
+
+
 
 }
 export default Detalles; 
---
+++ pokedex-app/src/pokemons.js
@@ -0,0 +1,173 @@
+
+const Pokemons = () => {
+
+const arrayPokemons = [
+    {   
+        "Img": "./img/aron.png",
+        "Name": "Aron",
+        "Id": "#304",
+        "About": {
+            "Weight": "60,0 kg",
+            "Height": "0,4 m",
+            "Moves": "Sturdy Rock-Head"
+        },
+        "INFO": "It eats iron ore- and sometimes railroad tracks - to build up the steel armor that protects its body",
+        "Base Stats": {
+            "HP": "050",
+            "ATK": "070",
+            "DEF": "100",
+            "SATK": "040",
+            "SDEF": "040",
+            "SPD": "030"
+        }
+    },
+
+    {
+        "Name": "Bulbasaur",
+        "Id": "#001",
+        "About": {
+            "Weight": "6,9 kg",
+            "Height": "0,7 m",
+            "Moves": "Chiorophyll Overgrow"
+        },
+        "INFO": "There is a plant seed on its back right from the day this pokemon is born. The seed slowly grows larger",
+        "Base Stats": {
+            "HP": "045",
+            "ATK": "049",
+            "DEF": "049",
+            "SATK": "065",
+            "SDEF": "065",
+            "SPD": "045"
+        }
+    },
+    {
+        "Name": "Butterfree",
+        "Id": "#012",
+        "About": {
+            "Weight": "32,0 kg",
+            "Height": "1,1 m",
+            "Moves": "Compound-Eyes-Tinted-Lens"
+        },
+        "INFO": "In battle, it flaps its wings at great speed to release highly toxic dust into the air",
+        "Base Stats": {
+            "HP": "060",
+            "ATK": "045",
+            "DEF": "050",
+            "SATK": "090",
+            "SDEF": "080",
+            "SPD": "070"
+        }
+    },
+    {
+        "Name": "Charmander",
+        "Id": "#004",
+        "About": {
+            "Weight": "8,5 kg",
+            "Height": "0,6 m",
+            "Moves": "Mega-Punch-Fire-Punch"
+        },
+        "INFO": "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail",
+        "Base Stats": {
+            "HP": "039",
+            "ATK": "052",
+            "DEF": "043",
+            "SATK": "060",
+            "SDEF": "050",
+            "SPD": "065"
+        }
+    },
+    {
+        "Name": "Ditto",
+        "Id": "#132",
+        "About": {
+            "Weight": "4,0 kg",
+            "Height": "0,3 m",
+            "Moves": "Limber Imposter"
+        },
+        "INFO": "It can reconstitute its entire cellular structure to change into what it sees, but it returns to normal when it relaxes",
+        "Base Stats": {
+            "HP": "048",
+            "ATK": "048",
+            "DEF": "048",
+            "SATK": "048",
+            "SDEF": "048",
+            "SPD": "048"
+        }
+    },
+    {
+        "Name": "Gastly",
+        "Id": "#092",
+        "About": {
+            "Weight": "0,1 kg",
+            "Height": "1,3 m",
+            "Moves": "Levitate"
+        },
+        "INFO": "Born from gases, anyone would falint if engulfed by its gaseous body, which contains poison",
+        "Base Stats": {
+            "HP": "030",
+            "ATK": "035",
+            "DEF": "030",
+            "SATK": "100",
+            "SDEF": "035",
+            "SPD": "080"
+        }
+    },
+    {
+        "Name": "Mew",
+        "Id": "#152",
+        "About": {
+            "Weight": "4,0 kg",
+            "Height": "0,4 m",
+            "Moves": "Synchronize"
+        },
+        "INFO": "When viewed throught a microscope, this pokemons short, fine, delicate hair can be seen",
+        "Base Stats": {
+            "HP": "100",
+            "ATK": "100",
+            "DEF": "100",
+            "SATK": "100",
+            "SDEF": "100",
+            "SPD": "100"
+        }
+    },
+    {
+        "Name": "Pikachu",
+        "Id": "#025",
+        "About": {
+            "Weight": "6,0 kg",
+            "Height": "0,4 m",
+            "Moves": "Mega-Punch-Pay-Day"
+        },
+        "INFO": "Pikachu that can generate powerful electricity have a cheek sacs that are extra soft and super stretchy",
+        "Base Stats": {
+            "HP": "035",
+            "ATK": "055",
+            "DEF": "040",
+            "SATK": "050",
+            "SDEF": "050",
+            "SPD": "090"
+        }
+    },
+    {
+        "Name": "Squirtle",
+        "Id": "#007",
+        "About": {
+            "Weight": "9,0 kg",
+            "Height": "0,5 m",
+            "Moves": "Torrent Rain-Dish"
+        },
+        "INFO": "When it retracts its long neck into its shell, it squirts out water with vigorous force",
+        "Base Stats": {
+            "HP": "044",
+            "ATK": "048",
+            "DEF": "065",
+            "SATK": "050",
+            "SDEF": "064",
+            "SPD": "043"
+        }
+    }
+    ]
+    console.log(arrayPokemons)
+}
+
+export default Pokemons;
