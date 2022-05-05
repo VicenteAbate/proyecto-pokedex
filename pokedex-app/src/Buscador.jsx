301e3678f18b3d885345bd7307a69c6a42d2a599
vicente <vicenteabate.22@gmail.com>
2022-05-05 15:28:26 -0300

    buscadortest

--- pokedex-app/src/Buscador.jsx
+++ pokedex-app/src/Buscador.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import Pokemons from "./pokemons";
 const Buscador = () => {
-    return(
-        <>
-            <div className="div-Buscador">
-                <input type="text" placeholder="Buscar Pokemon" />
-            </div>
-        
-        </>
-    )
+    const [searchTerm, setSearchTerm] = useState("")
+    return <div className="App"><input type="text" placeholder='Search' onChange={event =>{setSearchTerm(event.target.value)}} />
+    {Pokemons.filter((val)=>{
+      if(searchTerm == ""){
+        return val
+      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+      }
+    }).map((val, key)=>{
+      return (
+      <div className="user" key={key}>
+        <p>{val.name}</p>
+        </div>
+      )
+  
+    })}
+    </div>
 
 }
 
