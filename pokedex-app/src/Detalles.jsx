81529bd9565750627ab8cf66e74cc2ed5661a38c
natmacira <natimciraolo@gmail.com>
2022-05-05 08:54:35 -0300

    correcciones vistas en clase sobre la imprsión en pantalla de los elementos

--- pokedex-app/src/Detalles.jsx
+++ pokedex-app/src/Detalles.jsx
@@ -1,42 +1,48 @@
 import { useEffect, useState } from "react";
 import Pokemons from "./pokemons";
 
-const Detalles =()=> {
+const Detalles = () => {
 
     const [listaPokemons, setListaPokemons] = useState(null);
 
     useEffect(() => {
         const getData = async () => {
-    
+
             try {
-            const response = Pokemons();
-        
-            setListaPokemons(response);
-             console.log(response[0].Name);
-        
+                const response = Pokemons();
+
+                setListaPokemons(response);
+             
+
             } catch (error) {
-                 console.log("Hay un error en tu API");   
-                
-            }      
-                   
+                console.log("Hay un error en tu API");
+
             }
-            getData();
-        }, []);
 
-        return(
-            <>
-            <h1>hola</h1>
-            {/* <div>{response[0].Name}</div> */}
-            {/* 
-            <ul>{listaPokemons &&
-        listaPokemons.map((item, i) => {
-            <li key={i}>{item.Name}</li>
-        })}
-        </ul> */}
-            </>
-        )
+        }
+        getData();
+    }, []);
 
+    const handleClick = () =>{
+        const response = Pokemons();
+      
+        setListaPokemons(response);
+        console.log(listaPokemons);
+    }
 
+    console.log(listaPokemons);
+
+    return (
+        <>
+        {/* <h1>{listaPokemons[0].name}</h1> */}
+        <button onClick={handleClick}>mundo</button> 
+        <ul>
+            {listaPokemons && listaPokemons.map((item, index) => (<li key={index}>{item.name}</li>))
+            } 
+        </ul>
+
+        </>
+    ) 
 
 }
 export default Detalles; 
