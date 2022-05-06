5b75fdd4970c96a58e4ad2978d951421b0594410
vicente <vicenteabate.22@gmail.com>
2022-05-06 18:33:16 -0300

    Actualizacion Viernes 1830

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -3,6 +3,8 @@ import Buscador from './Buscador';
 import Detalles from './Detalles';
 import { useEffect, useState } from "react";
 import Pokemons from "./pokemons.js";
+import './BuscadorEstilos.css';
+
 
 
 function App(props) {
--- pokedex-app/src/Buscador.jsx
+++ pokedex-app/src/Buscador.jsx
@@ -2,32 +2,43 @@ import { useState } from "react";
 import Pokemons from "./pokemons.js";
 import pokeball from "./img/Pokeball.png"
 
+
+
 const Buscador = () => {
 
   const [searchTerm, setSearchTerm] = useState("")
 
   const pokemons = Pokemons()
 
-  const filterPokemons = (pokemones) => pokemones.filter((pokemon)=>  pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) )
-   
+  const filterPokemons = (pokemones) => pokemones.filter((pokemon) => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   const pokemonsFilter = filterPokemons(pokemons)
 
-  return <div className="App">
-    
-    
-    <h1><img id="Pokeball" src={pokeball} alt="" />Pokedex</h1>
-    <input type="text" placeholder='Search' onChange={event =>{setSearchTerm(event.target.value)}} />
-  {pokemonsFilter.map((pokemon, key)=>{
-    return (
-    <div className="user" key={key}>
-      {/* <img src={pokemon.img} alt="figura pokemon" /> */}
-      <p>{pokemon.id}<br/></p>
-      <img id="Img-pokemons-buscador" src={pokemon.img} alt="figura pokemon" /><br/>
-      <p>{pokemon.name}</p>
+  return <div className="Buscador-box">
+    <div className="titulo-h1-img">
+      <h1 className="titulo-pokedex-buscador"><img className="Pokeball-titulo" src={pokeball} alt="" />Pokedex</h1>
     </div>
-    )
+    <div className="input-buscador">
+      <input className="input-buscador-pokemons" type="text" placeholder='Search' onChange={event => { setSearchTerm(event.target.value) }} />
+    </div>
+    {pokemonsFilter.map((pokemon, key) => {
+      return (
+        
+        <div className="separacion-pokemons" key={key}>
+          <div className="id-buscador">
+            <p className="color-id-buscador">{pokemon.id}<br /></p>
+          </div>
+          <div className="pokemons-buscador">
+            <img className="Img-pokemons-buscador" src={pokemon.img} alt="figura pokemon" /><br />
+          </div>
+          <div className="nombre-buscador">
+            <h3 className="color-nombre--buscador">{pokemon.name}</h3>
+          </div>
+        </div>
+  
+      )
 
-  })}
+    })}
   </div>
 }
 export default Buscador;
