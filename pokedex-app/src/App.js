0dff6a9ccdd9e1e90685e668aa3b74dd967ba178
vicente <vicenteabate.22@gmail.com>
2022-05-11 14:51:27 -0300

    Ultima Actualizacion

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import Buscador from './Buscador';
 import Detalles from './Detalles';
-import { useEffect, useState } from "react";
-import Pokemons from "./pokemons.js";
-import './BuscadorEstilos.css';
-
-
 
 function App(props) {
-
-
   return (
     <>
-    <Buscador />
-    <Detalles /> 
+      <Buscador />
+      <Detalles />
 
     </>
   );
 }
-
 export default App;
